5e5f2078e86bb2d875ff87a5a085afc5ca09d4e5
Berkay Sahin <92673021+berkaysahiin@users.noreply.github.com>
2022-10-29 13:38:34 +0300

    Linked List

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,18 @@
+typedef struct Node {
+  int value;
+  struct Node* next;
+}Node;
+
+Node* linked_list_get_last_node(Node** root);
+int linked_list_has_loop(Node* root);
+int linked_list_length(Node* root);
+int linked_list_length_recursive(Node* root);
+void linked_list_print(Node* root);
+void linked_list_insert_start(Node** root, int value);
+void linked_list_insert_end(Node** root, int value);
+void linked_list_insert_after(Node* target_node, int value);
+void linked_list_remove_node(Node** target_node);
+void linked_list_remove_element(Node** root, int value);
+void linked_list_deallocate(Node** root);
+void linked_list_deallocate_recursive(Node** root);
+void linked_list_reverse(Node** root);
